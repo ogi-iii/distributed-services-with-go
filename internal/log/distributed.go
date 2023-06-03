0e1ff883a624a4fb9425abca0f51968e5fd139ee
ogi-iii <kazuki.aape.09@gmail.com>
2023-06-03 23:40:16 +0900

    [feat] WIP:chapter-08

---
+++ internal/log/distributed.go
@@ -0,0 +1,81 @@
+package log
+
+import (
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/hashicorp/raft"
+	raftboltdb "github.com/hashicorp/raft-boltdb"
+)
+
+type DistributedLog struct {
+	config Config
+	log    *Log
+	raft   *raft.Raft
+}
+
+func NewDistributedLog(dataDir string, config Config) (*DistributedLog, error) {
+	l := &DistributedLog{
+		config: config,
+	}
+	if err := l.setupLog(dataDir); err != nil {
+		return nil, err
+	}
+	if err := l.setupRaft(dataDir); err != nil {
+		return nil, err
+	}
+	return l, nil
+}
+
+func (l *DistributedLog) setupLog(dataDir string) error {
+	logDir := filepath.Join(dataDir, "log")
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		return err
+	}
+	var err error
+	l.log, err = NewLog(logDir, l.config)
+	return err
+}
+
+func (l *DistributedLog) setupRaft(dataDir string) error {
+	// finite-state machine that applies commands
+	fsm := &fsm{
+		log: l.log,
+	}
+	logDir := filepath.Join(dataDir, "raft", "log")
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		return err
+	}
+	logConfig := l.config
+	logConfig.Segment.InitialOffset = 1
+	// log store where Raft stores the history of commands
+	logStore, err := newLogStore(logDir, logConfig)
+	if err != nil {
+		return err
+	}
+	// stable store where Raft store the configurations of the cluster
+	stableStore, err := raftboltdb.NewBoltStore(
+		filepath.Join(dataDir, "raft", "stable"),
+	)
+	if err != nil {
+		return err
+	}
+	retain := 1
+	snapshotStore, err := raft.NewFileSnapshotStore(
+		filepath.Join(dataDir, "raft"),
+		retain,
+		os.Stderr,
+	)
+	if err != nil {
+		return err
+	}
+	maxPool := 5
+	timeout := 10 * time.Second
+	transport := raft.NewNetworkTransport(
+		l.config.Raft.StreamLayer,
+		maxPool,
+		timeout,
+		os.Stderr,
+	)
+}
