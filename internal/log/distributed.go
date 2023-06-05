d0d5219bcdd43311c4a1804b70c8011ee71c2e38
ogi-iii <kazuki.aape.09@gmail.com>
2023-06-05 18:53:31 +0900

    [feat] WIP:chapter-08

--- internal/log/distributed.go
+++ internal/log/distributed.go
@@ -1,12 +1,17 @@
 package log
 
 import (
+	"bytes"
+	"io"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/hashicorp/raft"
+	"google.golang.org/protobuf/proto"
+
 	raftboltdb "github.com/hashicorp/raft-boltdb"
+	api "github.com/ogi-iii/proglog/api/v1"
 )
 
 type DistributedLog struct {
@@ -80,4 +85,192 @@ func (l *DistributedLog) setupRaft(dataDir string) error {
 		timeout,
 		os.Stderr,
 	)
+	config := raft.DefaultConfig()
+	config.LocalID = l.config.Raft.LocalID
+	if l.config.Raft.HeartbeatTimeout != 0 {
+		config.HeartbeatTimeout = l.config.Raft.HeartbeatTimeout
+	}
+	if l.config.Raft.ElectionTimeout != 0 {
+		config.ElectionTimeout = l.config.Raft.ElectionTimeout
+	}
+	if l.config.Raft.LeaderLeaseTimeout != 0 {
+		config.LeaderLeaseTimeout = l.config.Raft.LeaderLeaseTimeout
+	}
+	if l.config.Raft.CommitTimeout != 0 {
+		config.CommitTimeout = l.config.Raft.CommitTimeout
+	}
+	l.raft, err = raft.NewRaft(
+		config,
+		fsm,
+		logStore,
+		stableStore,
+		snapshotStore,
+		transport,
+	)
+	if err != nil {
+		return err
+	}
+	hasState, err := raft.HasExistingState(
+		logStore,
+		stableStore,
+		snapshotStore,
+	)
+	if err != nil {
+		return err
+	}
+	// startup a cluster as initial node: subsequently added nodes do not bootstrap a cluster
+	if l.config.Raft.Bootstrap && !hasState {
+		config := raft.Configuration{
+			Servers: []raft.Server{
+				{
+					ID:      config.LocalID,
+					Address: transport.LocalAddr(),
+				},
+			},
+		}
+		err = l.raft.BootstrapCluster(config).Error()
+	}
+	return err
+}
+
+func (l *DistributedLog) Append(record *api.Record) (uint64, error) {
+	res, err := l.apply(
+		AppendRequestType,
+		&api.ProduceRequest{
+			Record: record,
+		},
+	)
+	if err != nil {
+		return 0, err
+	}
+	return res.(*api.ProduceResponse).Offset, nil
+}
+
+func (l *DistributedLog) apply(reqType RequestType, req proto.Message) (
+	interface{},
+	error,
+) {
+	var buf bytes.Buffer
+	_, err := buf.Write([]byte{byte(reqType)})
+	if err != nil {
+		return nil, err
+	}
+	b, err := proto.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+	_, err = buf.Write(b)
+	if err != nil {
+		return nil, err
+	}
+	timeout := 10 * time.Second
+	// append log with Raft replication
+	future := l.raft.Apply(buf.Bytes(), timeout) // RequestType + data
+	// Raft replication error handling
+	if future.Error() != nil {
+		return nil, future.Error()
+	}
+	res := future.Response()
+	// FSM Apply() error handling
+	if err, ok := res.(error); ok {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (l *DistributedLog) Read(offset uint64) (*api.Record, error) {
+	return l.log.Read(offset)
+}
+
+// finite-state machine in Raft
+var _ raft.FSM = (*fsm)(nil)
+
+type fsm struct {
+	log *Log
+}
+
+type RequestType uint8 // 1 byte
+
+const (
+	AppendRequestType RequestType = 0
+)
+
+// apply committed Raft log (command history)
+func (f *fsm) Apply(record *raft.Log) interface{} {
+	buf := record.Data
+	reqType := RequestType(buf[0]) // extract first 1 byte
+	switch reqType {
+	case AppendRequestType:
+		return f.applyAppend(buf[1:])
+	}
+	return nil
+}
+
+func (f *fsm) applyAppend(b []byte) interface{} {
+	var req api.ProduceRequest
+	err := proto.Unmarshal(b, &req) // bytes -> protobuf
+	if err != nil {
+		return err
+	}
+	offset, err := f.log.Append(req.Record) // append a record to local log
+	if err != nil {
+		return err
+	}
+	return &api.ProduceResponse{Offset: offset}
+}
+
+func (f *fsm) Snapshot() (raft.FSMSnapshot, error) {
+	r := f.log.Reader()
+	return &snapshot{reader: r}, nil
+}
+
+var _ raft.FSMSnapshot = (*snapshot)(nil)
+
+type snapshot struct {
+	reader io.Reader
+}
+
+// save the snapshot to sink
+func (s *snapshot) Persist(sink raft.SnapshotSink) error {
+	// copy from reader data to sink disc
+	if _, err := io.Copy(sink, s.reader); err != nil {
+		_ = sink.Cancel()
+		return err
+	}
+	return sink.Close()
+}
+
+// delete the snapshot from sink
+func (s *snapshot) Release() {}
+
+func (f *fsm) Restore(r io.ReadCloser) error {
+	b := make([]byte, lenWidth)
+	var buf bytes.Buffer
+	for i := 0; ; i++ {
+		_, err := io.ReadFull(r, b)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		}
+		size := int64(enc.Uint64(b))
+		if _, err := io.CopyN(&buf, r, size); err != nil {
+			return err
+		}
+		record := &api.Record{}
+		if err := proto.Unmarshal(buf.Bytes(), record); err != nil {
+			return err
+		}
+		if i == 0 {
+			f.log.Config.Segment.InitialOffset = record.Offset
+			if err := f.log.Reset(); err != nil {
+				return err
+			}
+		}
+		if _, err := f.log.Append(record); err != nil {
+			return err
+		}
+		buf.Reset()
+	}
+	return nil
 }
